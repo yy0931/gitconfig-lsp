54cba54b0d0529fcb35bf169cde7a3d5434dcafa
yy0931 <54441600+yy0931@users.noreply.github.com>
2021-12-24 10:53:31 +0900

    Improve the git object browser

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,58 @@
-import * as path from 'path'
-import * as vscode from 'vscode'
+import path from 'path'
+import vscode from 'vscode'
 import { LanguageClient, TransportKind } from 'vscode-languageclient/node'
 import { execFileSync } from "child_process"
+import fs from "fs"
+
+const escapeHTML = (s: string) => s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;")
+const git = (cwd: vscode.Uri, ...args: string[]) => {
+    try {
+        return execFileSync("git", args, { cwd: path.dirname(cwd.fsPath) }).toString()
+    } catch (err: any) {
+        throw err
+    }
+}
+
+const hashPattern = /(?<![\da-f])[\da-f]{40}(?![\da-f])/g
+
+// returns file:**/.git/objects/xx/xxx...xx or file:**/.git/objects/pack/pack-.*?xxxxx..xx
+const locateGitObject = (originUri: vscode.Uri, hash: string): vscode.Uri | null => {
+    let gitUri = originUri
+    for (let i = 0; !gitUri.path.endsWith(".git"); i++) {
+        if (!gitUri.path.includes(".git") || i > 256) { return null }
+        gitUri = vscode.Uri.joinPath(gitUri, "..")
+    }
+
+    const raw = vscode.Uri.joinPath(gitUri, "objects", hash.slice(0, 2), hash.slice(2))
+    if (fs.existsSync(raw.fsPath)) {
+        return raw
+    }
+
+    const packfileDir = vscode.Uri.joinPath(gitUri, "objects", "pack")
+    for (const filename of fs.readdirSync(packfileDir.fsPath)) {
+        const entry = vscode.Uri.joinPath(packfileDir, filename)
+        if (git(packfileDir, "verify-pack", "-v", entry.fsPath).split("\n").some((line) => line.startsWith(hash))) {
+            return entry.with({ query: hash })
+        }
+    }
+
+    return null
+}
+
+const openObject = (repo: vscode.Uri, hash: string) => {
+    const uri = locateGitObject(repo, hash)
+    if (uri === null) { return null }
+    return vscode.commands.executeCommand("vscode.openWith", uri, "gitconfig-lsp.cat-file")
+}
+
+type DocumentLink = vscode.DocumentLink | vscode.DocumentLink & { uri: vscode.Uri, hash: string }
 
 export const activate = (context: vscode.ExtensionContext) => {
+    const webpackOptions: vscode.WebviewOptions = {
+        enableScripts: true,
+        localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, "webview")],
+    }
+
     const serverModule = context.asAbsolutePath(path.join('dist', 'server.js'))
     context.subscriptions.push(
         new LanguageClient(
@@ -20,58 +69,131 @@ export const activate = (context: vscode.ExtensionContext) => {
                 ],
             },
         ).start(),
-        vscode.window.registerCustomEditorProvider("gitconfig-lsp.git-cat-file", {
+        vscode.window.registerCustomEditorProvider("gitconfig-lsp.cat-file", {
             openCustomDocument(uri) {
+                if (uri.scheme !== "file" || !fs.existsSync(uri.fsPath)) { return undefined }
                 return { uri, dispose: () => { } }
-            },
-            resolveCustomEditor(document, webviewPanel) {
+            }, // **/objects/xx/xx..xx **/pack/pack-.*?xxx
+            async resolveCustomEditor(document, webviewPanel) {
                 try {
-                    const escapeHTML = (s: string) => s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;")
-                    const hash = document.uri.path.split("/").slice(-2).join("")
-                    const content = escapeHTML(execFileSync("git", ["cat-file", "-p", hash], { cwd: path.dirname(document.uri.fsPath) }).toString())
-                    webviewPanel.webview.options = {
-                        enableScripts: true,
-                        localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, "webview")],
-                    }
-                    const template = (options: { head: string, body: string }) => `\
+                    const hash = document.uri.query || document.uri.path.split("/").slice(-2).join("")
+                    const content = escapeHTML(git(document.uri, "cat-file", "-p", hash))
+                    webviewPanel.webview.options = webpackOptions
+                    switch (git(document.uri, "cat-file", "-t", hash).trim()) {
+                        case "commit":
+                        case "tree":
+                            webviewPanel.webview.html = (await vscode.workspace.fs.readFile(vscode.Uri.joinPath(context.extensionUri, "webview/custom-editor.html"))).toString()
+                                .replace(`{{title}}`, hash)
+                                .replace(`{{code}}`, content.replace(hashPattern, (x) => `<a href="#">${x}</a>`))
+                            webviewPanel.webview.onDidReceiveMessage((hash: string) => { openObject(document.uri, hash) })
+                            break
+                        default:// "blob"
+                            webviewPanel.webview.html = `\
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>git-cat-file</title>
-    ${options.head}
+    <title>git cat-file</title>
+    <link href="${webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "webview/prism.css"))}" rel="stylesheet" />
 </head>
 <body>
-    ${options.body}
+    <pre><code class="language-txt">${content}</code></pre>
+    <script src="${webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "webview/prism.js"))}"></script>
 </body>
 </html>
 `
-                    switch (execFileSync("git", ["cat-file", "-t", hash], { cwd: path.dirname(document.uri.fsPath) }).toString().trim()) {
-                        case "commit":
-                        case "tree":
-                            webviewPanel.webview.html = template({
-                                head: ``,
-                                body: `<pre><code style="color: var(--vscode-editor-foreground);">${content.replace(/[\da-f]{40}/g, (x) => `<a href="#">${x}</a>`)}</code></pre>` +
-                                    `<script>const vscode = acquireVsCodeApi(); for (const a of document.querySelectorAll("a")) { a.addEventListener("click", (e) => { e.preventDefault(); vscode.postMessage(e.currentTarget.innerText) }) }</script>`,
-                            })
-                            webviewPanel.webview.onDidReceiveMessage((hash: string) => {
-                                vscode.commands.executeCommand("vscode.openWith", vscode.Uri.joinPath(document.uri, "../..", hash.slice(0, 2), hash.slice(2)), "gitconfig-lsp.git-cat-file");
-                            })
-                            break
-                        default:// "blob"
-                            webviewPanel.webview.html = template({
-                                head: `<link href="${webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "webview/prism.css"))}" rel="stylesheet" />`,
-                                body: `<pre><code class="language-txt">${content}</code></pre>` +
-                                    `<script src="${webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "webview/prism.js"))}"></script>`,
-                            })
                     }
-                } catch (err) {
-                    vscode.window.showErrorMessage(err + "")
+                } catch (err: any) {
+                    vscode.window.showErrorMessage(err.stack)
+                    throw err
                 }
             },
         } as vscode.CustomReadonlyEditorProvider, { supportsMultipleEditorsPerDocument: true, webviewOptions: { enableFindWidget: true, retainContextWhenHidden: true } }),
+        vscode.window.registerCustomEditorProvider("gitconfig-lsp.ls-files--stage", {
+            openCustomDocument(uri) {
+                if (uri.scheme !== "file" || !fs.existsSync(uri.fsPath)) { return undefined }
+                return { uri, dispose: () => { } }
+            },
+            async resolveCustomEditor(document, webviewPanel) {
+                try {
+                    webviewPanel.webview.options = webpackOptions
+                    webviewPanel.webview.html = webviewPanel.webview.html = (await vscode.workspace.fs.readFile(vscode.Uri.joinPath(context.extensionUri, "webview/custom-editor.html"))).toString()
+                        .replace(`{{title}}`, path.basename(document.uri.path))
+                        .replace(`{{code}}`, git(document.uri, "ls-files", "--stage").replace(hashPattern, (x) => `<a href="#">${x}</a>`))
+                    webviewPanel.webview.onDidReceiveMessage((hash: string) => { openObject(document.uri, hash) })
+                } catch (err: any) {
+                    vscode.window.showErrorMessage(err.stack)
+                    throw err
+                }
+            },
+        } as vscode.CustomReadonlyEditorProvider, { supportsMultipleEditorsPerDocument: true, webviewOptions: { enableFindWidget: true, retainContextWhenHidden: true } }),
+        vscode.window.registerCustomEditorProvider("gitconfig-lsp.verify-pack-v", {
+            openCustomDocument(uri) {
+                if (uri.scheme !== "file" || !fs.existsSync(uri.fsPath)) { return undefined }
+                return { uri, dispose: () => { } }
+            },
+            async resolveCustomEditor(document, webviewPanel) {
+                try {
+                    webviewPanel.webview.options = webpackOptions
+                    webviewPanel.webview.html = webviewPanel.webview.html = (await vscode.workspace.fs.readFile(vscode.Uri.joinPath(context.extensionUri, "webview/custom-editor.html"))).toString()
+                        .replace(`{{title}}`, path.basename(document.uri.path))
+                        .replace(`{{code}}`, git(document.uri, "verify-pack", "-v", document.uri.fsPath).replace(hashPattern, (x) => `<a href="#">${x}</a>`))
+                    webviewPanel.webview.onDidReceiveMessage((hash: string) => { openObject(document.uri, hash) })
+                } catch (err: any) {
+                    vscode.window.showErrorMessage(err.stack)
+                    throw err
+                }
+            },
+        } as vscode.CustomReadonlyEditorProvider, { supportsMultipleEditorsPerDocument: true, webviewOptions: { enableFindWidget: true, retainContextWhenHidden: true } }),
+        vscode.languages.registerDocumentLinkProvider("git-internals", {
+            provideDocumentLinks(textDocument) {
+                try {
+                    let uri = textDocument.uri
+                    for (let i = 0; ; i++) {
+                        if (!textDocument.uri.path.includes(".git") || i > 256) { return }
+                        uri = vscode.Uri.joinPath(uri, "..")
+                        if (uri.path.endsWith(".git")) { break }
+                    }
+
+                    const links = new Array<DocumentLink>()
+
+                    // hash
+                    const text = textDocument.getText()
+                    for (const match of text.matchAll(hashPattern)) {
+                        if (match.index === undefined) { continue }
+                        links.push({
+                            range: new vscode.Range(textDocument.positionAt(match.index), textDocument.positionAt(match.index + 40)),
+                            uri,
+                            hash: match[0].trim(),
+                        })
+                    }
+
+                    // ref
+                    for (const match of text.matchAll(/refs(\/[^ \n\t\/]+)+/g)) {
+                        if (match.index === undefined) { continue }
+                        links.push({
+                            range: new vscode.Range(textDocument.positionAt(match.index), textDocument.positionAt(match.index + match[0].length)),
+                            target: vscode.Uri.joinPath(uri, match[0].trim())
+                        })
+                    }
+
+                    return links
+                } catch (err: any) {
+                    vscode.window.showErrorMessage(err.stack)
+                    throw err
+                }
+            },
+            resolveDocumentLink(link) {
+                if ("uri" in link) {
+                    const uri = locateGitObject(link.uri, link.hash)
+                    if (uri === null) { return }
+                    link.target = uri
+                }
+                return link
+            }
+        } as vscode.DocumentLinkProvider<DocumentLink>),
     )
 }
 
